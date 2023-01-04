53f031a99360dbc70ac447ef14fe62206d6cb884
GabrielMR1974 <gabrielmonesruiz@outlook.com>
2023-01-04 15:35:45 -0300

    Modificacion script

--- clonrepos.sh
+++ clonrepos.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+USUARIO=$1
+
 # Obtener la lista de repositorios públicos del usuario
-curl -s https://api.github.com/users/edgaregonzalez/repos | jq -r '.[].name' > repo_list.txt
+curl -s https://api.github.com/users/$USUARIO/repos | jq -r '.[].name' > repo_list.txt
 
 # Iterar a través de la lista de repositorios y clonar cada uno
 while read repo; do
-  git clone https://github.com/edgaregonzalez/$repo.git
+  git clone https://github.com/$USUARIO/$repo.git
 done < repo_list.txt
 
 # Eliminar el archivo temporal con la lista de repositorios
